30bb9ed830ce29b9a498341b7cf136291fa19dcd
olivia <ojeglum@gmail.com>
2021-09-21 18:10:13 -0700

    commit tree 5df30fa38b5dbcaa85f61e60238059e90eefbda8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickup) {
+    let distance;
+    if (pickup >= 42) {
+       return distance = pickup - 42;
+    } if (pickup <= 42) {
+        return distance = 42 - pickup;
+    }
+}
+
+function distanceFromHqInFeet (beginning) {
+    let distance = Math.abs((beginning - 42)*264);
+    return distance;
+}
+
+function distanceTravelledInFeet (start, end) {
+    let distance = Math.abs((start-end)*264);
+    return distance;
+}
+
+function calculatesFarePrice (start, end) {
+    let distance = Math.abs((start-end)*264);
+        if (distance <= 400) {
+            return distance * 0;
+    }   if (distance > 400 && distance < 2000) {
+            return (distance - 400) * 0.02;
+    }   if (distance > 2000 && distance < 2500) {
+            return 25;
+    }   if (distance > 2500) {
+            return "cannot travel that far";
+    }
+}
